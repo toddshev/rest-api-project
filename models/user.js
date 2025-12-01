e111243022f87d2d8d964c70643180535a9fcc0d
Todd Shevlin <todd.shevlin@gmail.com>
2025-12-01 18:15:47 -0500

    401

--- models/user.js
+++ models/user.js
@@ -74,8 +74,10 @@ module.exports = (sequelize, DataTypes) => {
           },
         },
       set(val){
-        const hashedPassword = bcrypt.hashSync(val, 10);
-        this.setDataValue('password', hashedPassword);
+        if (val){
+          const hashedPassword = bcrypt.hashSync(val, 10);
+          this.setDataValue('password', hashedPassword);
+        }
       },
     },
   }, {
